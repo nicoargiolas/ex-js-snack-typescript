4903a9fa143ff55078e7fce39eba1f5a0504b95d
nicoargiolas <argiolas.nicola00@gmail.com>
2025-06-27 10:31:46 +0200

    Snack 2

--- src/main.ts
+++ src/main.ts
@@ -24,3 +24,37 @@ if (typeof response === "string") {
 } else {
   console.log("Tipo non supportato");
 }
+
+
+// 🏆 Snack 2
+// Crea un type alias Dipendente che rappresenta un lavoratore con i seguenti dati:
+// nome → stringa
+// cognome → stringa
+// annoNascita → numero
+// sesso → Può essere solo "m" o "f".
+// anniDiServizio (array di numeri, es. [2014, 2015, 2017, 2018])
+
+// 🎯 BONUS
+// Il type alias Dipendente, ha anche i seguenti dati:
+// emailAziendale → Email assegnata al dipendente (non si può modificare)
+// contratto → Specifica il tipo di contratto del dipendente, con valori limitati a “indeterminato”, “determinato” o “freelance”.
+
+type Dipendente = {
+  nome: string,
+  cognome: string,
+  annoNascita: number,
+  sesso: "m" | "f",
+  anniDiServizio: number[],
+  readonly emailAziendale: string,
+  contratto: "indeterminato" | "determinato" | "freelance"
+}
+
+const dipendente: Dipendente = {
+  nome: "Nicola",
+  cognome: "Argiolas",
+  annoNascita: 2000,
+  sesso: "m",
+  anniDiServizio: [2025],
+  emailAziendale: "boolean@boolean.it",
+  contratto: "determinato"
+}
